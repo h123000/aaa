f62c3969dfbdc569ec81a81c8046c588fa37322f
h123000 <101681300+h123000@users.noreply.github.com>
2022-03-16 14:48:30 +0800

    Update git.java

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -9,5 +9,6 @@ public class git {
         System.out.println("master test!");
         System.out.println("hot - fix test!");
         System.out.println("share github");
+        System.out.println("pull test");
     }
 }
